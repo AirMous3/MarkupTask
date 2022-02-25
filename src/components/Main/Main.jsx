35fb2d7701d2073ad0692e67c939148486fa0e75
AirMous3 <airmouselol@gmail.com>
2022-02-25 15:29:22 +0300

    remove unused constant

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,59 +1,58 @@
 import React from "react";
 import {
-  ContentDescription,
-  ContentOffer,
-  ContentStats,
-  ContentTitle,
-  MainContent,
-  MainImages,
-  MainWrapper,
-  SectionWrapper,
+    ContentDescription,
+    ContentOffer,
+    ContentStats,
+    ContentTitle,
+    MainContent,
+    MainImages,
+    MainWrapper,
+    SectionWrapper,
 } from "./components";
-import { GetStartedForm } from "../GetStartedForm/GetStartedForm";
-import { Stat } from "../Stat/Stat";
-import { useTheme } from "styled-components";
+import {GetStartedForm} from "../GetStartedForm/GetStartedForm";
+import {Stat} from "../Stat/Stat";
 
 export const Main = () => {
-  const theme = useTheme();
-  return (
-    <SectionWrapper>
-      <MainWrapper>
-        <MainContent>
-          <ContentTitle>Booking of doctor’s appointment</ContentTitle>
+    return (
+        <SectionWrapper>
+            <MainWrapper>
+                <MainContent>
+                    <ContentTitle>Booking of doctor’s appointment</ContentTitle>
 
-          <ContentOffer>
-            Don’t Miss Our Exclusive Patient <span>Special</span>
-          </ContentOffer>
+                    <ContentOffer>
+                        Don’t Miss Our Exclusive Patient <span>Special</span>
+                    </ContentOffer>
 
-          <ContentDescription>
-            We want to make sure that everyone has access to natural and
-            effective care. With our special, you’ll receive a consultation and
-            a digital posture assesment.
-          </ContentDescription>
+                    <ContentDescription>
+                        We want to make sure that everyone has access to natural and
+                        effective care. With our special, you’ll receive a consultation
+                        and
+                        a digital posture assesment.
+                    </ContentDescription>
 
-          <GetStartedForm />
+                    <GetStartedForm/>
 
-          <ContentStats>
-            <Stat
-              number="240"
-              description="Qualified Doctors & Medical Specialists"
-            />
+                    <ContentStats>
+                        <Stat
+                            number="240"
+                            description="Qualified Doctors & Medical Specialists"
+                        />
 
-            <Stat
-              number="1.456"
-              description="Medical Tests Done For Our Patients"
-              accent
-            />
+                        <Stat
+                            number="1.456"
+                            description="Medical Tests Done For Our Patients"
+                            accent
+                        />
 
-            <Stat
-              number="1M+"
-              description="Years of Experience The Medical Field"
-            />
-          </ContentStats>
-        </MainContent>
+                        <Stat
+                            number="1M+"
+                            description="Years of Experience The Medical Field"
+                        />
+                    </ContentStats>
+                </MainContent>
 
-        <MainImages />
-      </MainWrapper>
-    </SectionWrapper>
-  );
+                <MainImages/>
+            </MainWrapper>
+        </SectionWrapper>
+    );
 };
