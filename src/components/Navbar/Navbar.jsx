b7ad9cdd842656900de3a9a7903a7a5005f5bfd6
AirMous3 <airmouselol@gmail.com>
2022-03-04 10:44:21 +0300

    add navBar config

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { UlWrapper } from './components';
+import { config } from './config';
 
 export const Navbar = React.memo(() => (
   <nav>
     <UlWrapper>
-      <li>
-        <a href="#home">Home</a>
-      </li>
-      <li>
-        <a href="#services">Services</a>
-      </li>
-      <li>
-        <a href="#clinic">Clinic</a>
-      </li>
-      <li>
-        <a href="#doctors">Doctors</a>
-      </li>
-      <li>
-        <a href="#contact">Contact</a>
-      </li>
+      {config.map(({ path, label }) => (
+        <li key={path}>
+          <a href={path}>{label}</a>
+        </li>
+      ))}
     </UlWrapper>
   </nav>
 ));
---
+++ src/components/Navbar/config.js
@@ -0,0 +1,7 @@
+export const config = [
+  { path: '#home', label: 'Home' },
+  { path: '#services', label: 'Services' },
+  { path: '#clinic', label: 'Clinic' },
+  { path: '#doctors', label: 'Doctors' },
+  { path: '#contact', label: 'Contact' },
+];
